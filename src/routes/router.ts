7b6f57fbd87450dc0472a17a168edb6a99101420
sumitsharma16 <52009661+sumitsharma16@users.noreply.github.com>
2025-11-28 16:19:21 +0530

    Update router.ts

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("/", (req, res) => res.json({ message: "Hello from ECS v1 🎉" }));
+router.get("/", (req, res) => res.json({ message: "Hello from ECS v2 🎉" }));
 
 router.get("/health", (req, res) => {
   res.status(200).json({ status: "ok" });
